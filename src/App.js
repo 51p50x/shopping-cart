4affa4dee12287db8b6b9389813cd2ae752fe299
51p50x <51plog50@gmail.com>
2022-03-14 16:22:44 -0600

    sc-1: begin of the base app
     * created products and product components
    
     * Added Products component to App component
    
     Added inline style to the products and product components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import {Component} from 'react'
-
+import Productos from './components/Productos'
 class App extends Component {
+  state = {
+    productos:[
+      {name: 'Tomate', price: 1500, img:'/productos/tomate.jpg'},
+      {name: 'Arbejas', price: 2500, img:'/productos/arbejas.jpg'},
+      {name: 'Lechuga', price: 500, img:'/productos/lechuga.jpg'},
+    ]
+  }
+
   render(){
     return(
       <div>
-        <h1>Welcome to my shopping cart!</h1>
+        <Productos
+          agregarAlCarro={()=>console.log('no hace nada')}
+          productos={this.state.productos}
+        />
       </div>
     )
   }
