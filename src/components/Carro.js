d57e78bd6d2f0dff6aede2f1d2f8b836ad3f242e
51p50x <51plog50@gmail.com>
2022-03-15 11:26:59 -0600

    sc-8: Created function that changes the number of items for the bubbleAlert in the cart
     * Created function getNumber in BubbleAlert Component
    
     * Added function getNumber in BubbleAlert
    
     * Added value for BubbleAlert Component in Cart component

--- src/components/Carro.js
+++ src/components/Carro.js
@@ -22,7 +22,7 @@ class Carro extends Component{
         return(
             <div>
                 <span style={styles.bubble}>
-                    <BubbleAlert/>
+                    <BubbleAlert value={11}/>
                 </span>
                 <button style={styles.carro}>
                     Carro
