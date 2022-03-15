3a262793aa7ed74215c7f392ebd1be5a77d63eeb
51p50x <51plog50@gmail.com>
2022-03-15 12:15:12 -0600

    sc-9 started to work in the add to cart method
     * agregarAlCarro property was created, this property would be in charge to add the products to the shopping cart

--- src/App.js
+++ src/App.js
@@ -9,17 +9,30 @@ class App extends Component {
       {name: 'Tomate', price: 1500, img:'/productos/tomate.jpg'},
       {name: 'Arbejas', price: 2500, img:'/productos/arbejas.jpg'},
       {name: 'Lechuga', price: 500, img:'/productos/lechuga.jpg'},
-    ]
+    ],
+    carro:[
+
+    ],
+  }
+
+  agregarAlCarro = (producto) =>{
+      return this.setState({
+        carro:this.state.carro.concat({
+          ...producto,
+          cantidad: 1,
+        })
+      })
   }
 
   render(){
+    console.log(this.state.carro)
     return(
       <div>
         <Navbar/>
         <Layout>
           <Title/>
           <Productos
-            agregarAlCarro={()=>console.log('no hace nada')}
+            agregarAlCarro={this.agregarAlCarro}
             productos={this.state.productos}
           />
         </Layout>
